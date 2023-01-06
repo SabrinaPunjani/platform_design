99aef0c01f39940d5e1e37181f8d3c114fff0e7c
sabrinapunjani <sabrinaxjp@gmail.com>
2023-01-06 14:30:05 -0500

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,288 @@
+// Copyright 2020 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:platform_design/main.dart';
+
+// import 'bluetooth/bluetooth-off-screen.dart';
+import 'dart:async';
+import 'dart:math';
+import 'song_detail_tab.dart';
+import 'utils.dart';
+import 'widgets.dart';
+
+final FlutterBlue flutterBlue = FlutterBlue.instance;
+// enum BluetoothState { disabled, enabled, connected, disconnected, loading }
+
+void main() {
+  runApp(const MyAdaptingApp());
+}
+
+bool checkBluetoothStatus() {
+  return true;
+}
+
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+  final FlutterBlue flutterBlue = FlutterBlue.instance;
+  final List<BluetoothDevice> devicesList = <BluetoothDevice>[];
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<MyHomePage> {
+  int bottomNavbarIndex = 0;
+
+  _addDeviceTolist(final BluetoothDevice device) {
+    if (!widget.devicesList.contains(device)) {
+      setState(() {
+        widget.devicesList.add(device);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.flutterBlue.connectedDevices
+        .asStream()
+        .listen((List<BluetoothDevice> devices) {
+      for (BluetoothDevice device in devices) {
+        _addDeviceTolist(device);
+      }
+    });
+    widget.flutterBlue.scanResults.listen((List<ScanResult> results) {
+      for (ScanResult result in results) {
+        _addDeviceTolist(result.device);
+      }
+    });
+    widget.flutterBlue.startScan();
+  }
+
+  // FOR BLUETOOTH
+  ListView _buildListViewOfDevices() {
+    List<Widget> containers = <Widget>[];
+    // widget.devicesList
+    for (BluetoothDevice device in widget.devicesList) {
+      containers.add(
+        SizedBox(
+          height: 50,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(device.name == '' ? '(unknown device)' : device.name,
+                        style: const TextStyle(fontWeight: FontWeight.bold)
+                        // style: const TextStyle(
+                        //   fontWeight: FontWeight.bold,
+                        // ),
+                        ),
+                    Text(device.id.toString()),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                // color: Colors.blue,
+                child: const Text(
+                  'Connect',
+                  // style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return ListView(
+      padding: const EdgeInsets.all(16),
+      children: <Widget>[
+        ...containers,
+      ],
+    );
+  }
+class SongsTab extends StatefulWidget {
+  static const title = 'Connect to Glasses (or Home?)';
+  static const androidIcon = Icon(Icons.home);
+  static const iosIcon = Icon(CupertinoIcons.music_note);
+
+  const SongsTab({super.key, this.androidDrawer});
+
+  final Widget? androidDrawer;
+
+  @override
+  State<SongsTab> createState() => _SongsTabState();
+}
+
+class _SongsTabState extends State<SongsTab> {
+  static const _itemsLength = 50;
+
+  final _androidRefreshKey = GlobalKey<RefreshIndicatorState>();
+
+  late List<MaterialColor> colors;
+  late List<String> songNames;
+
+  @override
+  void initState() {
+    _setData();
+    super.initState();
+  }
+
+  void _setData() {
+    colors = getRandomColors(_itemsLength);
+    songNames = getRandomNames(_itemsLength);
+  }
+
+  Future<void> _refreshData() {
+    return Future.delayed(
+      // This is just an arbitrary delay that simulates some network activity.
+      const Duration(seconds: 2),
+      () => setState(() => _setData()),
+    );
+  }
+
+  Widget _listBuilder(BuildContext context, int index) {
+    if (index >= _itemsLength) return Container();
+
+    // Show a slightly different color palette. Show poppy-ier colors on iOS
+    // due to lighter contrasting bars and tone it down on Android.
+    final color = defaultTargetPlatform == TargetPlatform.iOS
+        ? colors[index]
+        : colors[index].shade400;
+
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Hero(
+        tag: index,
+        child: HeroAnimatingSongCard(
+          song: songNames[index],
+          color: color,
+          heroAnimation: const AlwaysStoppedAnimation(0),
+          onPressed: () => Navigator.of(context).push<void>(
+            MaterialPageRoute(
+              builder: (context) => SongDetailTab(
+                id: index,
+                song: songNames[index],
+                color: color,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _togglePlatform() {
+    if (defaultTargetPlatform == TargetPlatform.iOS) {
+      debugDefaultTargetPlatformOverride = TargetPlatform.android;
+    } else {
+      debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+    }
+
+    // This rebuilds the application. This should obviously never be
+    // done in a real app but it's done here since this app
+    // unrealistically toggles the current platform for demonstration
+    // purposes.
+    WidgetsBinding.instance.reassembleApplication();
+  }
+
+  // ===========================================================================
+  // Non-shared code below because:
+  // - Android and iOS have different scaffolds
+  // - There are different items in the app bar / nav bar
+  // - Android has a hamburger drawer, iOS has bottom tabs
+  // - The iOS nav bar is scrollable, Android is not
+  // - Pull-to-refresh works differently, and Android has a button to trigger it too
+  //
+  // And these are all design time choices that doesn't have a single 'right'
+  // answer.
+  // ===========================================================================
+  Widget _buildAndroid(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(SongsTab.title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () async =>
+                await _androidRefreshKey.currentState!.show(),
+          ),
+          IconButton(
+            icon: const Icon(Icons.shuffle),
+            onPressed: _togglePlatform,
+          ),
+        ],
+      ),
+      drawer: widget.androidDrawer,
+      body: RefreshIndicator(
+        key: _androidRefreshKey,
+        onRefresh: _refreshData,
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          itemCount: _itemsLength,
+          itemBuilder: _listBuilder,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildIos(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        CupertinoSliverNavigationBar(
+          trailing: CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: _togglePlatform,
+            child: const Icon(CupertinoIcons.shuffle),
+          ),
+        ),
+        CupertinoSliverRefreshControl(
+          onRefresh: _refreshData,
+        ),
+        SliverSafeArea(
+          top: false,
+          sliver: SliverPadding(
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                _listBuilder,
+                childCount: _itemsLength,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(context) {
+    return PlatformWidget(
+      androidBuilder: _buildAndroid,
+      iosBuilder: _buildIos,
+    );
+  }
+}
