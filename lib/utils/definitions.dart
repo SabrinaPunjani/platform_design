aaf799dc271f8afbb27bbc965488ced94939cdb9
Felix <51464337+felixha00@users.noreply.github.com>
2023-01-17 17:55:13 -0500

    added some system status enums

---
+++ lib/utils/definitions.dart
@@ -0,0 +1,7 @@
+enum SystemStatus {
+  off, // Glasses are off
+  idle, // Glasses are in idle mode (not connected to Bluetooth)
+  connected, // Glasses are connected via Bluetooth and ready to track
+  running, // Glasses are currently running
+  loading, // Glasses are loading
+}
